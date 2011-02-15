591e295ab54baed33c5c451fb1f01c4f2d31fea0
Feth Arezki <feth@tuttu.info>
2011-02-15 23:16:02 +0100

    revamp ui to have a load button in it

--- simuleds/loader.py
+++ simuleds/loader.py
@@ -1,81 +1,13 @@
-from imp import find_module, load_module
-from os.path import split
-from sys import argv, stderr
-from traceback import print_exc
+from sys import argv
 
-from PyQt4.QtGui import QApplication, QFileDialog, QMessageBox
+from PyQt4.QtGui import QApplication
 
-from .simuleds import Interface, Simardui
-
-
-def simfactory(name, filename):
-    """
-    Builds a sim from a filename
-    """
-    setup = None
-    loop = None
-
-    if filename.endswith('.py'):
-        module = filename[:-3]
-    else:
-        module = filename
-    path, module = split(module)
-
-    plugin_info = find_module(module, [path,])
-    loaded = load_module(name, *plugin_info)
-
-    Simobject = type(name, (Simardui, ), {'setup': loaded.setup, 'loop': loaded.loop})
-
-    return Simobject
-
-
-def getsim():
-    """
-    Interacts with user to get a sim plugin
-    """
-    while True:
-        filename = unicode(QFileDialog.getOpenFileName())
-        if not filename:
-            QMessageBox.warning(
-                None, "Abort simuleds",
-                "Aborting, you did not specify a file."
-                )
-            return
-        print "opening file", filename
-
-        try:
-            simklass = simfactory('my pony sim', filename)
-        except (SyntaxError, TypeError), err:
-            print_exc(stderr)
-            QMessageBox.warning(
-                None, "Error",
-                "File '%s' looks like an invalid Python source." % filename
-                )
-            continue
-        except Exception, err:
-            print_exc(stderr)
-            QMessageBox.warning(None, "Error", unicode(err))
-            continue
-        return simklass
-
+from .simuleds import Interface
 
 def main():
 
     app = QApplication(argv)
-
-    simklass = getsim()
-    if not simklass:
-        return
-
-    #set up ui
     frame = Interface()
     frame.show()
-
-    #bind to my ui
-    frame.setsim(simklass())
-
     app.exec_()
 
-
-if __name__ == '__main__':
-    main()
--- simuleds/simuleds.py
+++ simuleds/simuleds.py
@@ -1,9 +1,14 @@
+from traceback import format_exc
+from imp import find_module, load_module
+from os.path import split
+
 from PyQt4.QtCore import QObject, QThread, SIGNAL
+from PyQt4.QtGui import QFileDialog, QMessageBox
 from PyQt4.QtGui import QFrame
 
 from . import api
 from .leds_ui import Ui_Frame
-from .api import NOTUSED, OUTPUT, INPUT, SimException
+from .api import delay, NOTUSED, OUTPUT, INPUT, SimException
 
 
 _PINSIGNAL = SIGNAL('value changed')
@@ -49,19 +54,26 @@ class Simardui(QObject):
         self.pins = tuple(Pin() for index in xrange(api.ARDUINO_DIGITAL_PIN_NB))
         self.started = self.alive = False
 
-    def start(self):
+    def reset(self):
         self.started = False
-        if self.alive:
-            self.log('<b>reset!</b>')
-            return
-        self.live()
+        self.log('<b>reset!</b>')
+
+    def stop(self):
+        self.alive = False
+        self.started = False
 
     def isresetting(self):
         return not self.started
 
-    def live(self):
+    def start(self):
+        for pin in self.pins:
+            pin.mode = OUTPUT
+            pin.digitalWrite(True)
+            delay(50)
+            pin.digitalWrite(False)
+            pin.mode = NOTUSED
         self.alive = True
-        while True:
+        while self.alive:
             self._setup()
             while self.started:
                 self.loop()
@@ -76,8 +88,6 @@ class Simardui(QObject):
         self.pins[index].mode = mode
 
     def _setup(self):
-        global pinMode
-        pinMode = self.pinMode
         self.setup()
         self.started = True
 
@@ -86,14 +96,43 @@ class Simardui(QObject):
 
 
 class ArduiThread(QThread):
-    def __init__(self, method, *args, **kwargs):
-        self.method = method
+    def __init__(self, start, stop, *args, **kwargs):
+        self.start_fun = start
+        self.stop_fun = stop
         self.args = args
         self.kwargs = kwargs
         QThread.__init__(self, None)
 
     def run(self):
-        self.method(*self.args, **self.kwargs)
+        self.start_fun(*self.args, **self.kwargs)
+
+    def stop(self):
+        self.stop_fun()
+
+def simfactory(name, filename):
+    """
+    Builds a sim from a filename
+    """
+    if filename.endswith('.py'):
+        module = filename[:-3]
+    else:
+        module = filename
+    path, module = split(module)
+
+    plugin_info = find_module(module, [path,])
+    loaded = load_module(name, *plugin_info)
+
+    if not hasattr(loaded, 'setup') or not hasattr(loaded, 'loop'):
+        raise SimException(
+            "Please give me a Python source with a setup AND a loop functions"
+            )
+
+    Simobject = type(
+        name, (Simardui, ),
+        {'setup': loaded.setup, 'loop': loaded.loop}
+        )
+
+    return Simobject
 
 
 class Interface(QFrame):
@@ -101,22 +140,108 @@ class Interface(QFrame):
         QFrame.__init__(self, parent)
         self.design = Ui_Frame()
         self.design.setupUi(self)
+        self.thread = None
+        self._setupsignals()
+        self.old_thread = None
+        self.sim = None
+
+    def _setupsignals(self):
+        #ui signals -> load a firmware
+        self.connect(self.design.load, SIGNAL('clicked()'), self.loadfile)
 
     def getboxbynum(self, num):
         return getattr(self.design, 'led_%d' % num)
 
     def setsim(self, sim):
+        if self.thread:
+            self.thread.stop()
+            self.thread.terminate()
+            self.thread.wait()
+
+        if self.sim:
+            self._disconnectsim(sim)
+        self._connectsim(sim)
+
+        self.old_thread = self.thread
+        self.thread = ArduiThread(sim.start, sim.stop)
+        self.thread.start()
+        self.sim = sim
+
+    def reset(self):
+        if not self.sim:
+            return
+        self.sim.reset()
+
+    def _connectsim(self, sim):
         #ui signals -> logs
         QObject.connect(sim, _LOOPMSGSIGNAL, self.design.log.append)
 
         #ui signals -> reset button on the proto
-        QObject.connect(self.design.start, SIGNAL('clicked()'), sim.start)
+        QObject.connect(self.design.start, SIGNAL('clicked()'), self.reset)
 
         #signals to set box values
         for index in xrange(api.ARDUINO_DIGITAL_PIN_NB):
             box = self.getboxbynum(index)
             QObject.connect(sim.pins[index], _PINSIGNAL, box.setChecked)
 
-        self.thread = ArduiThread(sim.start)
-        self.thread.start()
+    def _disconnectsim(self, sim):
+        #ui signals -> logs
+        QObject.disconnect(sim, _LOOPMSGSIGNAL, self.design.log.append)
+
+        #ui signals -> reset button on the proto
+        QObject.disconnect(self.design.start, SIGNAL('clicked()'), sim.start)
+
+        #signals to set box values
+        for index in xrange(api.ARDUINO_DIGITAL_PIN_NB):
+            box = self.getboxbynum(index)
+            QObject.disconnect(sim.pins[index], _PINSIGNAL, box.setChecked)
+
+    def log(self, message, style=''):
+        if style != '':
+            style = 'style="%s"' % style
+        self.design.log.append("<span %s>%s</span><br/>" % (style, message))
+
+    def loadfile(self):
+        simklass = self._loadfile()
+        if not simklass:
+            self.log("Cancel firmware load.")
+            return
+        else:
+            try:
+                self.setsim(simklass())
+            except:
+                self.err(format_exc())
+                raise
+
+    def err(self, message):
+        self.log(message, style='color=#ff0000;')
+
+    def _loadfile(self):
+        """
+        Interacts with user to get a sim plugin
+        """
+        while True:
+            filename = unicode(QFileDialog.getOpenFileName())
+            if not filename:
+                QMessageBox.warning(
+                    None, "Abort simuleds",
+                    "Aborting, you did not specify a file."
+                    )
+                return
+            self.log("opening file: %s." % filename)
+
+            try:
+                simklass = simfactory('my pony sim', filename)
+            except (SyntaxError, TypeError), err:
+                self.err(format_exc())
+                QMessageBox.warning(
+                    None, "Error",
+                    "File '%s' looks like an invalid Python source." % filename
+                    )
+                continue
+            except Exception, err:
+                self.err(format_exc())
+                QMessageBox.warning(None, "Error", unicode(err))
+                continue
+            return simklass
 
