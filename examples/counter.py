bb1ddd6599bc0fd3fe80b2272f7e0e6819c9e0bd
Feth Arezki <feth@tuttu.info>
2011-02-16 13:15:58 +0100

    provide (hopefully working) cpp equivalent code
    this is untested

--- examples/counter.py
+++ examples/counter.py
@@ -1,3 +1,48 @@
+"""
+example of a binary counter from 0 to 31.
+
+Produces:
+00000
+00001
+00010
+...
+11111
+
+cpp version
+-----------
+
+int PIN_NB = 5;
+int COMBINATIONS = 1 << 5;
+
+void setup()
+  {
+  for (int index=0 ; index < PIN_NB ; index++)
+    {
+    pinMode(index, OUTPUT);
+    }
+
+  }
+
+
+void loop()
+  {
+  for (int number=0 ; number < COMBINATIONS ; number++)
+    {
+    for (int index=0 ; number < PIN_NB ; index++)
+      {
+      int pinvalue = 1 << index;
+      if (pinvalue & number)
+        {
+        digitalWrite(index, HIGH);
+        } else {
+        digitalWrite(index, LOW);
+        }
+      }
+    delay(500);
+  }
+
+"""
+
 from simuleds.api import OUTPUT, delay
 
 
