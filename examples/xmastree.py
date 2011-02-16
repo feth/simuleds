a2a88833c30ff8ca12c1a99a2dbfa7d21577e8bb
Feth Arezki <feth@tuttu.info>
2011-02-16 10:49:57 +0100

    clearer variable name

--- examples/xmastree.py
+++ examples/xmastree.py
@@ -6,10 +6,10 @@ def setup(self):
         self.pinMode(index, OUTPUT)
 
 
-GROUPS = 3
+GROUP_SIZE = 3
 
 def loop(self):
-    for rotation in xrange(GROUPS):
+    for rotation in xrange(GROUP_SIZE):
 
         #HERE IS THE BREAK FOR THE RESET SWITCH
         if self.isresetting():
@@ -17,7 +17,7 @@ def loop(self):
         #END RESET DIRTY HACK
 
         for index in xrange(ARDUINO_DIGITAL_PIN_NB):
-            ledgroup = index % GROUPS
+            ledgroup = index % GROUP_SIZE
             outputvalue = ledgroup == rotation
             self.digitalWrite(index, outputvalue)
         delay(500)
